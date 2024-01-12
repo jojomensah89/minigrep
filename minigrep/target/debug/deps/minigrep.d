2899a806904ada74f0b684f85f40c105ca0f67ce
ebenezermensah <ebenezer.mensah@amalitech.org>
2024-01-12 14:01:42 +0000

    initial setup

---
+++ minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+C:\Users\Jojo\Desktop\learn-rust\minigrep\target\debug\deps\minigrep.exe: src\main.rs
+
+C:\Users\Jojo\Desktop\learn-rust\minigrep\target\debug\deps\minigrep.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+C:\Users\Jojo\Desktop\learn-rust\minigrep\target\debug\minigrep.exe: C:\Users\Jojo\Desktop\learn-rust\minigrep\src\main.rs
