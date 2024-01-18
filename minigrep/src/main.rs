065dbda43c449e6074f445c545a0e29547e630cd
ebenezermensah <ebenezer.mensah@amalitech.org>
2024-01-18 16:30:02 +0000

    reafctored the code with cleaner loops and iterators

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,21 +2,22 @@ use minigrep;
 use std::env;
 use std::process;
 
+//  cargo run -- to poem.txt > output.txt
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
-    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = minigrep::Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("in file  {}", config.file_path);
+    // println!("Searching for {}", config.query);
+    // println!("in file  {}", config.file_path);
     // println!("{:?}", args);
 
     // Read file
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
